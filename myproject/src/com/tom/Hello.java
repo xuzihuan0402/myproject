a8373412992a5982416f2a3d4bda57e3ea6812fd
user <user@es302n23>
2018-10-16 11:58:29 +0800

    第三次

--- myproject/src/com/tom/Hello.java
+++ myproject/src/com/tom/Hello.java
@@ -4,6 +4,27 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	
+		Student stu = new Student("Hank", 70, 90);
+		stu.print();
+		
+		System.out.println(3 <= 5);
+		
+		String s1 = new String ("abc");
+		String s2 = new String ("abc");
+		System.out.println(s1==s2);//false
+		System.out.println(s1.equals(s2));//true
+		
+		
+		
+		
+		
+		
+//		String name = "Hank";
+//		int english = 70;
+//		int math = 90;
+//		System.out.println(name + "\t" + english + "\t" + math + "\t" + (english + math)/2);
+		
 		System.out.println("Hello world");
 		Person P = new Person(49.5f, 1.7f);
 //		P.weight = 49.5f;
@@ -12,15 +33,16 @@ public class Hello {
 		Person F = new Person(65.5f, 1.6f);
 		System.out.println(F.bmi());
 		P.Hello();
+//		Person hank = new Person();
 //		Person jack = null;
-//		jack.Hello();
+//		jack.hello();
 		
 		
 		
 		
-		
-		
-		
+//		String name;
+//		name = "Hank";
+//		System.out.println(name);
 		
 		
 //		int age = 18;
--- myproject/src/com/tom/Student.java
+++ myproject/src/com/tom/Student.java
@@ -1,5 +1,20 @@
 package com.tom;
 
 public class Student {
+	String name;
+	int english;
+	int math;
+	
+	public Student (String name,int english,int math){
+		this.name = name;
+		this.english=english;
+		this.math=math;
+		
+	}
+	
+	public void print(){
+		System.out.println(name + "\t" + english + "\t" + math + "\t" + (english + math)/2);
+	}
 
+	
 }
