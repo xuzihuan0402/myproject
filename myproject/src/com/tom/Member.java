71de3f46197f7b804dc2da51af4e5418b19b6df5
user <user@es302n29>
2018-10-30 09:14:14 +0800

    10/30(1)

--- myproject/src/com/tom/Member.java
+++ myproject/src/com/tom/Member.java
@@ -1,14 +1,31 @@
 package com.tom;
 
+import java.util.Scanner;
+
 public class Member {
 	boolean adult;
 	int age;
 	String name;
 	String nickName;
-	public Member(int age, String name, String nickName);{
+	public Member(boolean adult){
+		this.adult = adult;
+	}
+	public Member(int age, String name, String nickname) {
 		this.age = age;
 		this.name = name;
-		this.nickName = nickName;
+		this.nickName = nickname;
+	}
+	
+	public void startInput(){
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Your age?");
+		age = scanner.nextInt();
+		scanner.nextLine();
+		System.out.print("Your name?");
+		name = scanner.nextLine();
+		System.out.print("Your nick name?");
+		nickName = scanner.nextLine();
+		System.out.println(age + "\t" + name + "\t" + nickName );
 		
 	}
 	
--- myproject/src/com/tom/SignUp.java
+++ myproject/src/com/tom/SignUp.java
@@ -3,28 +3,43 @@ package com.tom;
 import java.util.Scanner;
 
 public class SignUp {
-
 	public static void main(String[] args) {
-		System.out.println("Are you 18 ? (Y/N)");
+		System.out.println("Are you 18? (y/N)");
 		Scanner scanner = new Scanner(System.in);
 		String line = scanner.nextLine();
-		boolean adult = line.equalsIgnoreCase("y");
-		System.out.println(adult);
-		if(adult){
-			System.out.print("Your age?");
-			int age = scanner.nextInt();
-			scanner.nextLine();
-			System.out.print("Your name?");
-			String name = scanner.nextLine();
-			System.out.print("Your nick name?");
-			String nickName = scanner.nextLine();
-			System.out.println(age + "/" + name + "/" + nickName );
-		
-			
-		}else{
-			System.out.println("Thank you! Bye!");
+		boolean adult = line.toUpperCase().equals("Y");
+		Member member = new Member(adult);
+		if (member.adult) {
+			member.startInput();
+		}else {
+			System.out.println("Thank you!Bye!");
 		}
 		
 	}
-}
 	
+}
+
+//	public static void main(String[] args) {
+//		System.out.println("Are you 18 ? (Y/N)");
+//		Scanner scanner = new Scanner(System.in);
+//		String line = scanner.nextLine();
+//		boolean adult = line.equalsIgnoreCase("y");
+//		System.out.println(adult);
+//		if(adult){
+//			System.out.print("Your age?");
+//			int age = scanner.nextInt();
+//			scanner.nextLine();
+//			System.out.print("Your name?");
+//			String name = scanner.nextLine();
+//			System.out.print("Your nick name?");
+//			String nickName = scanner.nextLine();
+//			System.out.println(age + "/" + name + "/" + nickName );
+//		
+//			
+//		}else{
+//			System.out.println("Thank you! Bye!");
+//		}
+//		
+//	}
+//}
+//	
